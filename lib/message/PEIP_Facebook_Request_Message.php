e4cc85f4cd3b5ec9507ec9b793db4e4ff32e66df
Timo Michna <timomichna@yahoo.de>
2010-04-20 23:45:36 +0000

    added class doc tags

--- lib/message/PEIP_Facebook_Request_Message.php
+++ lib/message/PEIP_Facebook_Request_Message.php
@@ -1,5 +1,21 @@
 <?php
+/*
+ * This file is part of the PEIP_Facebook package.
+ * (c) 2010 Timo Michna <timomichna/yahoo.de>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+/**
+ * PEIP_Facebook_Request_Message 
+ *
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP_Facebook 
+ * @subpackage message 
+ * @extends PEIP_Generice_Message
+ * @implements PEIP_INF_Container, PEIP_INF_Message, PEIP_INF_Buildable
+ */
 class PEIP_Facebook_Request_Message 
 	extends PEIP_Generice_Message {
 		
--- lib/service/PEIP_Facebook_Service_Activator.php
+++ lib/service/PEIP_Facebook_Service_Activator.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ * This file is part of the PEIP_Facebook package.
+ * (c) 2010 Timo Michna <timomichna/yahoo.de>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * PEIP_Facebook_Service_Activator 
+ *
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP_Facebook  
+ * @subpackage service 
+ * @extends PEIP_ABS_Service_Activator
+ * @implements PEIP_INF_Connectable, PEIP_INF_Subscribable_Channel, PEIP_INF_Channel, PEIP_INF_Handler, PEIP_INF_Message_Builder
+ */
+
 class PEIP_Facebook_Service_Activator 
 	extends PEIP_ABS_Service_Activator {
 
